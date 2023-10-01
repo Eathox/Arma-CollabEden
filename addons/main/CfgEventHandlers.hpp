888c27768e1130b8358b6882cc8d1ad1048ac5f1
Eathox <pgaholster@gmail.com>
2023-10-01 16:31:24 +0200

    Remove useless recompile calls

--- addons/main/CfgEventHandlers.hpp
+++ addons/main/CfgEventHandlers.hpp
@@ -1,16 +1,13 @@
 // Remove recompile patch, recompiling is automatically disabled in release builds
 #ifndef IS_RELEASE_BUILD
-class Extended_PreInit_EventHandlers {
-    class ADDON {
-        init = QUOTE(call COMPILE_FILE(recompile));
-    };
-};
-
 class Cfg3DEN {
-	class EventHandlers {
-		class ADDON {
-			init = QUOTE(call COMPILE_FILE(recompile));
-		};
-	};
+    class EventHandlers {
+        class ADDON {
+            Init = QUOTE(call COMPILE_FILE(recompile));
+            OnMissionNew = QUOTE(call COMPILE_FILE(recompile));
+            OnMissionLoad = QUOTE(call COMPILE_FILE(recompile));
+            OnMissionPreviewEnd = QUOTE(call COMPILE_FILE(recompile));
+        };
+    };
 };
 #endif
